efe83b759bbd33a05f4bbfad8bdcdc8e93936639
Manjusenthilkumar <117365039+Manjusenthilkumar@users.noreply.github.com>
2025-06-21 19:42:23 +0530

    files added

---
+++ Number is present.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int n,search, count = 0;
+    printf("Enter the no of elements:");
+    scanf("%d",&n);
+    printf("Enter the search number:");
+    scanf("%d",&search);
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+         if(arr[i] == search){
+            printf("Present");
+            count = 1;
+            break;
+        }
+    }
+    if (count == 0){
+        printf("Not present");
+    }
+
+
+
+
+    return 0;
+}
